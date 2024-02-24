8ef6aab6744503a5c378cd9abc6f278b2e2b2558
Rafa <rafa272011@gmail.com>
2024-02-24 13:31:28 -0300

    actualizando y la app se llama corehome:

--- src/corehome/settings.py
+++ src/corehome/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+#import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#print(os.environ.get("WELCOME_MSG"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^oq6l#o@+ai_fi)j7krkh*v_+%!=5y%j!b*wuej^m_#81c7n#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True #Twingate
 
 ALLOWED_HOSTS = []
 
